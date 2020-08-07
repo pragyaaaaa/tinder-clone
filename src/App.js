597ff335e82ddeb5dba53ad1fd12ef658d203bc9
pragyaaaaa <pragyasabstracts@gmail.com>
2020-08-07 16:26:56 +0530

    --header-complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-     <h1>Let's tinder.</h1>
-     {/* header */}
-     <Header/>
-     {/* tinder cards */}
-     {/* buttons below tinder cards */}
-     {/* chat screen */}
-     {/* individual chat screen */}
+      {/* header */}
+      <Header />
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <h1>I'm chat page.</h1>
+          </Route>
+          {/* default route page should always be at the bottom */}
+          <Route path="/">
+            <h1>I'm homepage.</h1>
+          </Route>
+        </Switch>
+        {/* tinder cards */}
+        {/* buttons below tinder cards */}
+        {/* chat screen */}
+        {/* individual chat screen */}
+      </Router>
+
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -2,16 +2,21 @@ import React from 'react'
 import './Header.css';
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 import ChatIcon from '@material-ui/icons/Chat';
+import IconButton from '@material-ui/core/IconButton';
 //functional component: Header
-function Header(){
-    return(
+function Header() {
+    return (
         //BEM naming convention
         <div class="header">
-            <PersonOutlineIcon/>
+            <IconButton>
+                <PersonOutlineIcon className="header-icon" fontSize="large" />
+            </IconButton>
             <img
-            className="header-logo" 
-            src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png" alt="tinder-icon"></img>
-            <ChatIcon/>
+                className="header-logo"
+                src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png" alt="tinder-icon"></img>
+            <IconButton>
+                <ChatIcon className="header-icon" fontSize="large" />
+            </IconButton>
         </div>
     )
 }
