6cf2d0abda4b0e18d72f6b7170efe1da588d425a
pragyaaaaa <pragyasabstracts@gmail.com>
2020-08-08 07:29:08 +0530

    --chat

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,17 @@
 import React from 'react';
-function Chat(){
-    return(
+import "./Chat.css";
+import Avatar from '@material-ui/core/Avatar';
+function Chat({name, msg, dp, timeStamp}) {
+    return (
         <div className="chat">
+            <Avatar className="chat-image" alt={name} src={dp}></Avatar>
+            <div className="chat-details">
+                <h2>{name}</h2>
+                <p>{msg}</p>
+            </div>
+            <p className="chat-timestamp">{timeStamp}</p>
         </div>
-    )
+    );
 }
 
 export default Chat;
--- src/Chats.js
+++ src/Chats.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Chat from './Chat';
 function Chats(){
     return(
         <div className="chats">
+            <Chat
+                name="Sarah"
+                msg="What's up?"
+                timeStamp="35 min ago"
+                dp="">
+            </Chat>
         </div>
     )
 }
